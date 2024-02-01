e0d0fc00d85cd93fce3cbdf97d0baac02fb852b8
EloMorelo <127032342+EloMorelo@users.noreply.github.com>
2024-02-01 04:40:54 +0100

    Update Menu.js

--- Menu.js
+++ Menu.js
@@ -1,10 +1,19 @@
-var playerName;
+
+let playerName;
 
 document.addEventListener('DOMContentLoaded', () => {
     function saveName() {
-        playerName = document.getElementById("playerName");
+        playerName = document.getElementById("playerName").value;
+        localStorage.setItem('playerName', playerName);
         console.log("Player name saved: " + playerName);
     }
 
+    const buttons = document.querySelectorAll('.btn, .button-box button, .box-atk button, .box-items button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            clickSound.play();
+        });
+    });
     document.getElementById("saveButton").addEventListener("click", saveName);
 });
